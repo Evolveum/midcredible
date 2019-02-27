8ea3716e0db1c04db425ed51f44a224db3444738
matusmacik <mmacik@evolveum.com>
2019-02-27 13:13:27 +0100

    TableComparator small modifications

--- midcredible-framework/src/main/java/com/evolveum/midpoint/midcredible/framework/util/TableComparator.java
+++ midcredible-framework/src/main/java/com/evolveum/midpoint/midcredible/framework/util/TableComparator.java
@@ -8,7 +8,6 @@ import com.evolveum.midpoint.midcredible.framework.util.structural.Jdbc.Column;
 import groovy.lang.GroovyClassLoader;
 import org.apache.commons.io.FileUtils;
 import org.codehaus.groovy.jsr223.GroovyScriptEngineImpl;
-import org.intellij.lang.annotations.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,126 +38,112 @@ public class TableComparator {
     }
 
 
-//    	public Comparator compare() {
-//		try {
-//			executeComparison(setupComparator(comparatorPath)) ;
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (ScriptException e) {
-//			e.printStackTrace();
-//		} catch (IllegalAccessException e) {
-//			e.printStackTrace();
-//		} catch (InstantiationException e) {
-//			e.printStackTrace();
-//		}
-//		finally {
-//
-//			//TODO clean up
-//
-//		}
-//		return null;
-//	}
+    	public void compare(String outputFilePath, String identificator) {
+		try {
+			executeComparison(setupComparator(), outputFilePath, identificator) ;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ScriptException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+		finally {
 
-//    	protected void executeComparison(Comparator comparator) {
-//
-//        //TODO path from property file
-//        CsvReportPrinter reportPrinter = new CsvReportPrinter("./out.csv");
-//
-//		ResultSet oldRs = null;
-//		ResultSet newRs = null;
-//		ResultSetMetaData md =null;
-//		List attributeList=null;
-//
-//		try {
-//			oldRs = createResultSet(comparator.query(), oldResource);
-//			newRs = createResultSet(comparator.query(), newResource);
-//             md = oldRs.getMetaData();
-//
-//            int columns = md.getColumnCount();
-//
-//            for (int i = 0; i < columns; i++) {
-//                attributeList.add(md.getColumnName(i));
-//            }
-//
-//        } catch (SQLException e) {
-//            //TODO
-//            e.printStackTrace();
-//        }
-//
-//            Identity oldRow;
-//            Identity newRow;
-//
-//try {
-//	while (oldRs.next()) {
-//		oldRow = createIdentityFromRow(oldRs);
-//
-//		if (!newRs.next()) {
-//			// there's nothing left in new table, old table contains more rows than it should, mark old rows as "-"
-////			printCsvRow(printer, "-", oldRs);
-//			break;
-//		}
-//
-//		newRow = createIdentityFromRow(newRs);
-//		State state = comparator.compareIdentity(oldRow, newRow);
-//		switch (state) {
-//			case EQUAL:
-//				continue;
-//			case OLD_BEFORE_NEW:
-//				// new table contains row that shouldn't be there, mark new as "+"
-//                reportPrinter.printCsvRow(attributeList,newRow);
-//				//printCsvRow(printer, "+", newRs);
-//				break;
-//			case OLD_AFTER_NEW:
-//				// new table misses some rows obviously, therefore old row should be marked as "-"
-//				//printCsvRow(printer, "-", oldRs);
-//				break;
-//		}
-//	}
-//
-//	while (newRs.next()) {
-//		newRow = createMapFromRow(newRs);
-//		// these remaining records are not in old result set, mark new rows as "+"
-//		// todo print it out somehow
-//		//printCsvRow(printer, "+", newRs);
-//	}
-//} catch (SQLException e){
-//	// TODO
-//	LOG.error("Sql exception white iterating trough result set " + e.getLocalizedMessage());
-//}
-//
-//	}
+			//TODO clean up
 
-    private Map<Column, Object> createMapFromRow(ResultSet rs) throws SQLException {
-        ResultSetMetaData md = rs.getMetaData();
-        int columns = md.getColumnCount();
+		}
+	}
 
-        Map<Column, Object> map = new HashMap<>();
-        for (int i = 0; i < columns; i++) {
-            map.put(new Column(md.getColumnName(i), i), rs.getObject(i));
+    	protected void executeComparison(Comparator comparator, String outputFilePath, String identificator) {
+
+        //TODO path from property file
+        CsvReportPrinter reportPrinter = new CsvReportPrinter(outputFilePath);
+
+		ResultSet oldRs = null;
+		ResultSet newRs = null;
+		ResultSetMetaData md =null;
+		List attributeList=null;
+
+		try {
+			oldRs = createResultSet(comparator.query(), oldResource);
+			newRs = createResultSet(comparator.query(), newResource);
+            md = oldRs.getMetaData();
+
+            int columns = md.getColumnCount();
+
+            for (int i = 0; i < columns; i++) {
+                attributeList.add(md.getColumnName(i));
+            }
+
+        } catch (SQLException e) {
+            //TODO
+            e.printStackTrace();
         }
 
-        return map;
-    }
+            Identity oldRow;
+            Identity newRow;
 
-    private Map<Column, Object> createIdentityFromRow(ResultSet rs, String identifier) throws SQLException {
+try {
+	while (oldRs.next()) {
+		oldRow = createIdentityFromRow(oldRs, identificator);
+
+		if (!newRs.next()) {
+			// there's nothing left in new table, old table contains more rows than it should, mark old rows as "-"
+//			printCsvRow(printer, "-", oldRs);
+			break;
+		}
+
+		newRow = createIdentityFromRow(newRs, identificator);
+		State state = comparator.compareIdentity(oldRow, newRow);
+		switch (state) {
+			case EQUAL:
+				continue;
+			case OLD_BEFORE_NEW:
+				// new table contains row that shouldn't be there, mark new as "+"
+                reportPrinter.printCsvRow(attributeList,newRow);
+				//printCsvRow(printer, "+", newRs);
+				break;
+			case OLD_AFTER_NEW:
+                reportPrinter.printCsvRow(attributeList,oldRow);
+				// new table misses some rows obviously, therefore old row should be marked as "-"
+				//printCsvRow(printer, "-", oldRs);
+				break;
+		}
+	}
+
+	while (newRs.next()) {
+		//newRow = createMapFromRow(newRs);
+		// these remaining records are not in old result set, mark new rows as "+"
+		// todo print it out somehow
+		//printCsvRow(printer, "+", newRs);
+	}
+} catch (SQLException e){
+	// TODO
+	LOG.error("Sql exception white iterating trough result set " + e.getLocalizedMessage());
+} catch (IOException e) {
+    e.printStackTrace();
+}
+        }
+
+    private Identity createIdentityFromRow(ResultSet rs, String identifier) throws SQLException {
         Identity identity = new Identity(rs.getObject(identifier).toString());
         ResultSetMetaData md = rs.getMetaData();
         int columns = md.getColumnCount();
 
-        Map<Column, Object> map = new HashMap<>();
         for (int i = 0; i < columns; i++) {
-
             Attribute attr = new Attribute(md.getColumnName(i));
             Map<Diff, Collection> valueMap = new HashMap();
             attr.setInitialSingleValue(rs.getObject(i));
             identity.setAttributes(attr);
-            map.put(new Column(md.getColumnName(i), i), rs.getObject(i));
         }
 
-        return map;
+        return identity;
     }
 
-    private Comparator setupComparator(String comparatorPath) throws IOException, ScriptException, IllegalAccessException, InstantiationException {
+    private Comparator setupComparator() throws IOException, ScriptException, IllegalAccessException, InstantiationException {
         File file = new File(comparatorPath);
         String script = FileUtils.readFileToString(file, "utf-8");
 
